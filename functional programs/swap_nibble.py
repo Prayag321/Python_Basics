b92dd016d0a9ac16c94084f954ffb0c812bef9b9
PRAYAG <nanabhoir321@gmail.com>
2024-07-24 17:57:55 +0530

    [Prayag] Add: Python program to swap two nibbles and prind corresponding number

---
+++ functional programs/swap_nibble.py
@@ -0,0 +1,24 @@
+def nibble_swap(number):
+  """
+  Description:
+    This function simulates flipping a coin multiple times and returns the
+    percentages of heads and tails.
+
+  Parameters:
+    coin_flip: The number of times the coin is flipped. Must be a positive integer.
+
+  Returns:
+    A tuple containing the percentages of tails and heads, respectively.
+    If the input is invalid (less than 1), an error message is returned.
+  """
+  binary_number = convert_binary(number)
+
+  return binary_number
+def convert_binary(number):
+  pass
+
+def main():
+  number = int(input("Enter a number"))
+  print(nibble_swap(number))
+if __name__=="__main__":
+  main()
